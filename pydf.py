ac5f700f758d22e923dea4121df61aac8036c23d
madprogramer <themadprogramer@gmail.com>
2022-09-27 23:32:04 +0200

    Py3: Print statements

--- pydf.py
+++ pydf.py
@@ -46,7 +46,7 @@ def main():
     if pydf2json.os.path.isfile(args.pdf):
         x = open(args.pdf, 'rb').read()
     else:
-        print '%s does not exist.' % args.pdf
+        print ('%s does not exist.' % args.pdf)
         exit()
 
     # Check if dump location was specified and is valid
@@ -55,7 +55,7 @@ def main():
             pdf_object.dump_streams = True
             pdf_object.dump_loc = args.location
         else:
-            print '%s is not a valid dump directory location.' % args.Location
+            print ('%s is not a valid dump directory location.' % args.Location)
             exit()
 
     # Check if a password was passed:
@@ -68,30 +68,30 @@ def main():
     try:
         pdf_object.GetPDF(x)
     except pydf2json.MaxSizeExceeded as e:
-        print e
-        print 'See command help for max_size override.'
+        print (e)
+        print ('See command help for max_size override.')
         exit()
     except Exception as e:
-        print e
+        print (e)
         exit()
 
     if args.show_json:
         pdfstruct = pdf_object.expose_pdfstruct()
-        print json.dumps(pdfstruct, ensure_ascii=False, indent=4)
+        print (json.dumps(pdfstruct, ensure_ascii=False, indent=4))
 
     if not args.no_summary:
         summary = pdf_object.expose_summary()
         # Parse summary for presentation...
-        print 'Summary of PDF attributes:'
-        print '--------------------------\n'
+        print ('Summary of PDF attributes:')
+        print ('--------------------------\n')
         if summary['Encryption']['enabled']:
-            print '{:<29} {:<32}'.format('Encrypted:', 'True')
-            print '{:<29} {:<32}'.format('User Pass:', args.password)
-            print '{:<29} {:<32}'.format('File Key:', summary['Encryption']['file_key'])
-            print '{:<29} {:<32}'.format('Key Length:', str(summary['Encryption']['key_length']) + ' bits')
-            print '{:<29} {:<32}\n'.format('Algo:', summary['Encryption']['algorithm'])
+            print ('{:<29} {:<32}'.format('Encrypted:', 'True'))
+            print ('{:<29} {:<32}'.format('User Pass:', args.password))
+            print ('{:<29} {:<32}'.format('File Key:', summary['Encryption']['file_key']))
+            print ('{:<29} {:<32}'.format('Key Length:', str(summary['Encryption']['key_length']) + ' bits'))
+            print ('{:<29} {:<32}\n'.format('Algo:', summary['Encryption']['algorithm']))
         else:
-            print '{:<20} {:>10}'.format('Encrypted:', 'False')
+            print ('{:<20} {:>10}'.format('Encrypted:', 'False'))
         aa_num = 0
         aa_sections = {}
         for i in summary['Additional Actions']:
@@ -103,69 +103,69 @@ def main():
                         aa_sections[i] = []
                     aa_sections[i].append(j)
 
-        print '{:<20} {:>10}'.format('Additional Actions:', str(aa_num))
-        print '{:<20} {:>10}'.format('AcroForms:', str(summary['AcroForms']))
-        print '{:<20} {:>10}'.format('Embedded Files:', str(len(summary['EmbeddedFiles'])))
-        print '{:<20} {:>10}'.format('JS:', summary['JS'])
-        print '{:<20} {:>10}'.format('Launch:', str(len(summary['Launch'])))
-        print '{:<20} {:>10}'.format('Object Streams:', str(summary['Object Streams']))
-        print '{:<20} {:>10}'.format('OpenActions:', str(summary['OpenActions']))
-        print '{:<10} {:>20}'.format('Pages:', str(summary['Pages']))
+        print ('{:<20} {:>10}'.format('Additional Actions:', str(aa_num)))
+        print ('{:<20} {:>10}'.format('AcroForms:', str(summary['AcroForms'])))
+        print ('{:<20} {:>10}'.format('Embedded Files:', str(len(summary['EmbeddedFiles']))))
+        print ('{:<20} {:>10}'.format('JS:', summary['JS']))
+        print ('{:<20} {:>10}'.format('Launch:', str(len(summary['Launch']))))
+        print ('{:<20} {:>10}'.format('Object Streams:', str(summary['Object Streams'])))
+        print ('{:<20} {:>10}'.format('OpenActions:', str(summary['OpenActions'])))
+        print ('{:<10} {:>20}'.format('Pages:', str(summary['Pages'])))
 
         if len(summary['Arbitrary Data']) > 0:
-            print '\nArbitrary data detected (data outside any known PDF object)'
+            print ('\nArbitrary data detected (data outside any known PDF object)')
             for i in summary['Arbitrary Data']:
-                print '\t' + str(i)
+                print ('\t' + str(i))
 
         if len(summary['AcroForm Actions']) > 0:
-            print '\nAcroForm Actions'
+            print ('\nAcroForm Actions')
             for i in summary['AcroForm Actions']:
-                print '\t' + str(i)
+                print ('\t' + str(i))
 
         if len(summary['Launch']) > 0:
-            print '\nLaunches detected:'
+            print ('\nLaunches detected:')
             for i in summary['Launch']:
-                print '\t' + str(i)
+                print ('\t' + str(i))
 
         if len(summary['Names']) > 0:
-            print '\nName Tree trace entries:'
+            print ('\nName Tree trace entries:')
             if len(summary['EmbeddedFiles']) > 0:
-                print '\tEmbeddedFiles'
+                print ('\tEmbeddedFiles')
                 for i in summary['EmbeddedFiles']:
-                    print '\t\t' + str(i)
+                    print ('\t\t' + str(i))
             if len(summary['JavaScript']) > 0:
-                print '\n\tJavaScript'
+                print ('\n\tJavaScript')
                 for i in summary['JavaScript']:
-                    print '\t\t' + str(i)
+                    print ('\t\t' + str(i))
 
         if summary.has_key('Link Annotations'):
             if len(summary['Link Annotations']) > 0:
-                print '\nURIs in document:'
+                print ('\nURIs in document:')
                 tmp_link = []
                 for i in summary['Link Annotations']:
                     for j in summary['Link Annotations'][i]:
                         if not j['Link'] in tmp_link:
                             tmp_link.append(j['Link'])
                 for i in tmp_link:
-                    print '\t' + i
+                    print ('\t' + i)
 
         if aa_num > 0:
-            print '\nAdditional Actions (AA) detected:'
+            print ('\nAdditional Actions (AA) detected:')
             for i in aa_sections:
                 if i == 'acro_adds':
-                    print '\tAcroform Fields AA:'
+                    print ('\tAcroform Fields AA:')
                 if i == 'annot_adds':
-                    print '\tAnnotations AA:'
+                    print ('\tAnnotations AA:')
                 if i == 'page_adds':
-                    print '\tPage AA:'
+                    print ('\tPage AA:')
                 if i == 'cat_adds':
-                    print '\tCatalog AA:'
+                    print ('\tCatalog AA:')
                 for j in aa_sections[i]:
-                        print '\t\t', j
+                        print ('\t\t'), j
 
-        print '\nDocument Hashes:'
+        print ('\nDocument Hashes:')
         for i in summary['Document Hashes']:
-            print '\t{:<10} {:<0}'.format(i, summary['Document Hashes'][i])
+            print ('\t{:<10} {:<0}'.format(i, summary['Document Hashes'][i]))
 
 
         # New malware index stuff.
@@ -174,17 +174,17 @@ def main():
             tmp_index += format(i, 'x').zfill(2)
 
         if int(tmp_index, 16) > 0:
-            print '\nMisc indicators: ' # + str(int(tmp_index, 16))
+            print ('\nMisc indicators: ') # + str(int(tmp_index, 16))
             for i in range(0, len(summary['Malware Index'])):
                 tmp_num = summary['Malware Index'][i]
                 if tmp_num > 9:
                     if i == 7:
-                        print '\t' + str(tmp_num) + ' instances of unnecessary white space.'
+                        print ('\t' + str(tmp_num) + ' instances of unnecessary white space.')
                 if tmp_num > 0:
                     if i == -1: # Disabled with -1. Reenable by setting to 5
-                        print '\t' + str(tmp_num) + ' misaligned objects or cross-reference tables.'
+                        print ('\t' + str(tmp_num) + ' misaligned objects or cross-reference tables.')
                     if i == 6:
-                        print '\t' + str(tmp_num) + ' unnecessary encodings of named objects. Anti-analysis technique.'
+                        print ('\t' + str(tmp_num) + ' unnecessary encodings of named objects. Anti-analysis technique.')
 
 
 if __name__ == '__main__':
